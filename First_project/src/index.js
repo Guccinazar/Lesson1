1d318653f160ba25955c4d195947bc2674525c00
Guccinazar <guccinazar@gmail.com>
2021-09-14 21:59:03 +0300

    js_1

---
+++ First_project/src/index.js
@@ -0,0 +1,15 @@
+const name = "Andrii";
+const age = 24.5;
+const isAdult = true ;
+const something = null;
+const obj = {
+    name: "Nazar"
+}
+
+const bigNumber = 312154548812154144512123 //BigInt
+const symbol = Symbol();
+const func = function() {
+    console.log('Hello');
+}
+
+func ();
