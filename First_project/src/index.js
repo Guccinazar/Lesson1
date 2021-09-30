9e2a3a18c1edc73ce05441334bc5e51fbced5025
Guccinazar <guccinazar@gmail.com>
2021-09-30 23:41:21 +0300

    sleep

--- First_project/src/index.js
+++ First_project/src/index.js
@@ -1,150 +1,62 @@
 // Task 1
-// let firstNumber=prompt("Enter the beginning of the range","");
-// let lastNumber=prompt("Enter the end of the range","");
-// let sum=0;
- 
-// while(firstNumber < lastNumber){
-//        firstNumber++;
-//        sum+=firstNumber;    
+// Написать функцию, которая принимает 2 числа и возвращает -1, 
+// если первое меньше, чем второе; 1 – если первое больше, чем второе; и 0 – если числа равны.
+// let numb1 = +prompt('Enter the first number');
+// let numb2 = +prompt('Enter te second number')
+
+// function resultNumber (numb1 , numb2) {
+//     if (numb1 < numb2) {
+//         document.write(-1);
+//     } else if (numb1 > numb2) {
+//         document.write(1);
+//     } else {
+//         document.write(0);
 //     }
- 
-// alert(sum)
-
-// Task 2
-// let firstNumber= +prompt("Enter the first number","");
-// let lastNumber= +prompt("Enter the second number","");
-
-// while (lastNumber) {
-//   let divider = lastNumber;
-//   lastNumber = firstNumber % lastNumber;
-//   firstNumber = divider;
 // }
 
-// alert(`The largest common divis is ${firstNumber}`);
+// resultNumber(numb1, numb2);
+
+// Task 2 
+// Написать функцию, которая вычисляет факториал переданного ей числа.
+// let num = +prompt("Enter the number");
+// function factorial(num){
+//     for(var i = num - 1; i > 0; i--){
+//          num *= i;
+//      }
+//      return num;     
+//    }
+   
+// // alert(factorial(num));
 
 // Task 3
-// let number = +prompt('Please enter some number');
+// Написать функцию, которая принимает три отдельные цифры и превращает их в одно число. 
+// Например: цифры 1, 4, 9 превратятся в число 149.
 
-// const initialNumber = number;
+// let numb1 = prompt('Enter the first number');
+// let numb2 = prompt('Enter the second number');
+// let numb3 = prompt('Enter the third number');
 
-// document.write(`All dividers: \n`);
-
-// while (number > 0) {
-//     if (initialNumber % number === 0) {
-//         document.write(`${number}\n`);
-//     }
-//     number--;
+// function allTogether(numb1, numb2, numb3) {
+//     return numb1 + numb2 + numb3;
 // }
 
-// Task 4
-// let enterNumber = prompt('Please, enter any number');
-// let counting = 0;
+// alert(allTogether(numb1, numb2 , numb3));
 
-// for (let i=0; i < enterNumber.length; i++) {
-//     counting++;
-// }
+// Task 4 
+// Написать функцию, которая принимает длину и ширину прямоугольника и вычисляет его площадь.
+//  Если в функцию передали 1 параметр, то она вычисляет площадь квадрата.
 
-// alert(`The number you entered has ${counting} symbols`)
+// let width = prompt('Enter the width');
+// let height = prompt('Enter the height');
 
-// Task 5
-// let positiveNumberCount = 0;
-// let negativeNumberCount = 0;
-// let zeroNumberCount = 0;
-// let evenNumberCount = 0;
-// let oddNumberCount = 0;
-
-// for (i = 0; i < 10; i++) {
-//     let currentNumber = +prompt('Please enter 10 numbers')
-//     if (currentNumber > 0) {
-//         positiveNumberCount++;
-//     } else if (currentNumber <0) {
-//         negativeNumberCount++;
-//     } else {
-//         zeroNumberCount++;
-//     } 
-//     if (currentNumber % 2 == 0) {
-//         evenNumberCount++;
-//     } else {
-//         oddNumberCount++;
+// function countArea (width, height) {
+//     if (width != '' && height != '') {
+//         return width * height;
+//     } else if (width != '' && height == '') {
+//         return width * width;
+//     } else if (width == '' && height != '') {
+//         return height * height;
 //     }
 // }
 
-// document.write(`You are entered: ${positiveNumberCount} positiv numbers, ${negativeNumberCount} negativ numbers,
-// ${zeroNumberCount} zero numbers, ${evenNumberCount} even numbers, ${oddNumberCount} odd numbers`);
-
-// Task 6
-// do {
-//     let firstNumber = +prompt('Enter the first number');
-//     let sign = prompt('Choose a sign - + / *');
-//     let secondNumber = +prompt('Enter the second number');
-    
-//         switch (sign) {
-//             case '+':
-//                 alert(firstNumber + secondNumber);
-//                 break;
-//             case '-':
-//                 alert(firstNumber - secondNumber);
-//                 break;
-//             case '/':
-//                 alert(firstNumber / secondNumber);
-//                 break;
-//             case '*':
-//                 alert(firstNumber * secondNumber);
-//             default:
-//                 break;
-//         }
-// }
-//  while (confirm('Want to solve another example?'));
-
-// Task 7
-
-// let number = prompt('Enter a number!');
-// let shift = +prompt('Enter a shift number!');
-
-// number = number.slice(shift, number.length) + number.slice(0, shift);
-
-// alert(`The result of shift is: ${number}`);
-
-// Task 8
-// const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-// let currentDay = 0;
-
-// while (confirm(`${days[currentDay]}. Want to see the next day?`)) {
-//     currentDay = (currentDay + 1) % days.length;
-// }
-
-// Task 9
-
-// let x=prompt("Enter a number:",'');
-// let alertBody = '';
-// for (let i=1; i<10; i++) {
-//   alertBody += x + "*" + i +"="+x*i + '\n';
-// }
-// // alert(alertBody);
-
-// Task 10
-
-let minValue = 0;
-let maxValue = 100;
-let N = maxValue / 2;
-let isGame = true;
-
-let is
-do {
- let answer = prompt("Ваше число  ${N} ?, smaller/bigger/yes");
-
-  switch (answer) {
-    case 'smaller':
-     maxValue = N - 1;   
-     N = parseInt(minValue + ((maxValue - minValue) / 2));
-     break;
-    case 'bigger':
-     minValue = N + 1;   
-     N = parseInt(minValue + ((maxValue - minValue) / 2));
-     break;
-    default:
-        isGame = false;           
-  }
-} while (isGame)
-
-console.log('Your Number is: ${N}')
+// alert(countArea(width,height));
